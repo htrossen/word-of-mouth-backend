308f9319b7ecf139b2158359be004f84848f0cd2
HaileyTrossen <hailey.trossen@target.com>
2021-02-13 11:44:27 -0600

    wip

---
+++ src/test/kotlin/haileyj/wordofmouth/WordOfMouthApplicationTests.kt
@@ -0,0 +1,13 @@
+package haileyj.wordofmouth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WordOfMouthApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
