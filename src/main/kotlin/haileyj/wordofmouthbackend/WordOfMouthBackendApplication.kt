78154a1387a919ade3275e90852b91f55151ba79
HaileyTrossen <hailey.trossen@target.com>
2021-02-13 11:44:27 -0600

    wip

---
+++ src/main/kotlin/haileyj/wordofmouthbackend/WordOfMouthBackendApplication.kt
@@ -0,0 +1,11 @@
+package haileyj.wordofmouth
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WordOfMouthApplication
+
+fun main(args: Array<String>) {
+	runApplication<WordOfMouthApplication>(*args)
+}
